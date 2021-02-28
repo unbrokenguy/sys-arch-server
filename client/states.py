aee07e699d248365054ccc59516e2fb2fd2b2f32
Булат Хазиев <khazievbulatphanzilovich@gmail.com>
2021-03-01 00:42:53 +0300

    DownloadState.handle_download less complex, some code improvement

--- client/states.py
+++ client/states.py
@@ -58,58 +58,65 @@ class DownloadState(State):
         self.actions = dict.copy(base_actions)
         self.actions.update({"Выбрать категорию": StartState})
 
+    def file_response(self, response):
+        if "status" in response.text:
+            Tools.handle_response(response)
+            return False
+        elif "data" in response.text:
+            data = json.loads(response.text)
+            Tools.print_ok_message("Данные успешно получены.")
+            print(data["data"])
+            return False
+        return True
+
     def download(self, category, file_name):
         response = requests.get(
             url=f"{self.context.server_url}/download?file_type={category}&file_name={file_name}",
             stream=True,
         )
-        if "status" in response.text:
-            Tools.handle_response(response)
-        elif "data" in response.text:
-            data = json.loads(response.text)
-            Tools.print_ok_message("Данные успешно получены.")
-            p = Path(f"{self.context.storage_path}/{category}/")
-            p.mkdir(parents=True, exist_ok=True)
-            print(data["data"])
-        else:
+        if self.file_response(response):
             if response.status_code == 200:
-                p = Path(f"{self.context.storage_path}/{category}/")
-                p.mkdir(parents=True, exist_ok=True)
-                if not os.path.isfile(f"{p}/{file_name}"):
-                    with open(f"{p}/{file_name}", "wb+") as destination:
+                path = Path(f"{self.context.storage_path}/{category}/")
+                path.mkdir(parents=True, exist_ok=True)
+                if not os.path.isfile(f"{path}/{file_name}"):
+                    with open(f"{path}/{file_name}", "wb+") as destination:
                         for chunk in response:
                             destination.write(chunk)
-                Tools.print_ok_message(f"Файл успешно загружен в {p}/{file_name}")
+                Tools.print_ok_message(f"Файл успешно загружен в {path}/{file_name}")
 
-    def choises(self, choices):
+    def get_choises(self, choices):
         choices["choose"].extend(list(base_actions.keys()))
         choices = Tools.get_choose_dict(choices)
         Tools.print_choose_dict(choices)
         return choices
 
+    def handle_file_download(self, file_category):
+        response = requests.get(
+            url=f"{self.context.server_url}/download?file_type={file_category}"
+        )
+        files = json.loads(response.text)
+        files = self.get_choises(files)
+        file_name_input = input()
+        if files[str(file_name_input)] in base_actions.keys():
+            return base_actions[files[file_name_input]]
+        if file_name_input not in files.keys():
+            Tools.print_error("Пожалуйста введите корректные данные.")
+        else:
+            self.download(file_category, files[file_name_input])
+        return None
+
     def handle_download(self):
         categories = json.loads(
             requests.get(url=f"{self.context.server_url}/download").text
         )
-        categories = self.choises(categories)
+        categories = self.get_choises(categories)
         category_input = input()
-        if categories[category_input] in base_actions.keys():
-            return base_actions[categories[category_input]]
         if category_input not in categories.keys():
             Tools.print_error("Пожалуйста введите корректные данные.")
+        elif categories[category_input] in base_actions.keys():
+            return base_actions[categories[category_input]]
         else:
-            response = requests.get(
-                url=f"{self.context.server_url}/download?file_type={categories[category_input]}"
-            )
-            files = json.loads(response.text)
-            files = self.choises(files)
-            file_name_input = input()
-            if files[str(file_name_input)] in base_actions.keys():
-                return base_actions[files[file_name_input]]
-            if file_name_input not in files.keys():
-                Tools.print_error("Пожалуйста введите корректные данные.")
-            else:
-                self.download(categories[category_input], files[file_name_input])
+            return self.handle_file_download(categories[category_input])
         return None
 
     def action(self):
