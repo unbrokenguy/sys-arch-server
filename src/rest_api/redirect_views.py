c91b36726a86e65c3d091a08096e4dbfb59a30f8
Булат Хазиев <khazievbulatphanzilovich@gmail.com>
2021-04-05 21:42:39 +0300

    redirect_view change

--- src/rest_api/redirect_views.py
+++ src/rest_api/redirect_views.py
@@ -9,11 +9,16 @@ from django.http import HttpResponse, JsonResponse
 @csrf_exempt
 def auth(request, method):
     if request.method == "POST":
-        credentials = {"email": request.POST["email"], "password": request.POST["password"]}
-        res = requests.post(url=f"http://{os.getenv('AUTH_APP_IP')}/api/auth/{method}/", data=credentials)
+        credentials = {}
+        if method == "sign_in":
+            credentials = {"email": request.POST["email"], "password": request.POST["password"]}
+        elif method == "sign_up":
+            credentials = {"email": request.POST["email"], "password": request.POST["password"],
+                           "first_name": request.POST["first_name"], "last_name": request.POST["last_name"]}
+        res = requests.post(url=f"http://{os.getenv('AUTH_APP_IP')}/api/auth/{method}/", data=credentials, headers=request.headers)
         if res.status_code == 200:
             return JsonResponse(data=json.loads(res.text), status=200)
         else:
-            return HttpResponse(status=res.status_code)
+            return HttpResponse(res.text, status=res.status_code)
     else:
         return HttpResponse(status=404)
