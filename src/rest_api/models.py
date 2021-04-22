3a0b041a3bf4ef4ab795f41e09bc0e2fbd8dbc4b
unbrokenguy <30524457+unbrokenguy@users.noreply.github.com>
2021-04-22 19:06:43 +0300

    strategy pattern (#1)

--- src/rest_api/models.py
+++ src/rest_api/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from rest_api.utils import file_upload
 
 
-class Category(models.Model):
+class DataField(models.Field):
+    description = "String or File"
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class Category(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False)
 
 
-class File(models.Model):
+class Data(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    data = models.FileField(upload_to=file_upload, null=False)
-
-
-class UserInput(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    data = models.CharField(max_length=1000)
+    data = DataField()
--- src/rest_api/urls.py
+++ src/rest_api/urls.py
@@ -1,10 +1,8 @@
-from rest_api.views import UserInputViewSet, FileViewSet, CategoryViewSet, DataViewSet
+from rest_api.views import CategoryViewSet, DataViewSet
 from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
-router.register(r"file", FileViewSet, basename="api-file")
-router.register(r"user_input", UserInputViewSet, basename="api-user-input")
 router.register(r"data", DataViewSet, basename="api-data")
 router.register(r"category", CategoryViewSet, basename="api-category")
 urlpatterns = router.urls
